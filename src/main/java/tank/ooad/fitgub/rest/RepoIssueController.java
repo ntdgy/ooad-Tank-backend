eb8ee6224f017202e46753574cce859c4c9c4cb1
dgy <ntdgy2001@gmail.com>
2022-10-05 17:09:24 +0800

    remove all files about issue

--- src/main/java/tank/ooad/fitgub/rest/RepoIssueController.java
+++ src/main/java/tank/ooad/fitgub/rest/RepoIssueController.java
@@ -18,25 +18,7 @@ public class RepoIssueController {
     @Autowired
     private RepoIssueService repoIssueService;
 
-    @PostMapping("/api/repo/issue/create")
-    @RequireLogin
-    public Return<Void> createIssue(@RequestParam int repoId, @RequestParam String title, HttpSession session) {
-        int userId = (int) AttributeKeys.USER_ID.getValue(session);
-        if (userId == 0)
-            return new Return<>(ReturnCode.LOGIN_REQUIRED);
-        repoIssueService.createIssue(repoId, title, userId);
-        return Return.OK;
-    }
 
-    @PostMapping("/api/repo/issue/addComment")
-    @RequireLogin
-    public Return<Void> addComment(@RequestParam int issueId, @RequestParam String content, HttpSession session) {
-        int userId = (int) AttributeKeys.USER_ID.getValue(session);
-        if (userId == 0)
-            return new Return<>(ReturnCode.LOGIN_REQUIRED);
-        repoIssueService.insertComment(issueId, userId, content);
-        return Return.OK;
-    }
 
 
 }
--- src/main/java/tank/ooad/fitgub/service/RepoIssueService.java
+++ src/main/java/tank/ooad/fitgub/service/RepoIssueService.java
@@ -9,33 +9,6 @@ public class RepoIssueService {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    public int createIssue(int repoId, String title, int userId) {
-        jdbcTemplate.update("""
-                               insert into issue(repo_id, repo_issue_id, issuer_user_id, title)
-                               select ?, repo.next_issue_id, ?, ?
-                               from repo where repo.id = ?;
-                               """, repoId, userId, title, repoId);
-        jdbcTemplate.update("""
-                               update repo
-                               set next_issue_id = next_issue_id + 1
-                               where id = ?;
-                               """, repoId);
-        return 1;
-    }
 
-    public int insertComment(int issueId, int userId, String content) {
-        jdbcTemplate.update("""
-                               insert into issue_comment(issue_id, comment_id, sender_user_id, content)
-                               select ?, next_comment_id, ?, ?
-                               from issue
-                               where id = ?;
-                               """, issueId, userId, content, issueId);
-        jdbcTemplate.update(""" 
-                                  update issue
-                                 set next_comment_id = next_comment_id + 1
-                                 where id = ?;
-                                 """, issueId);
-        return 1;
-    }
 
 }
