921a2eebad3739c560226d6059eda632a137716e
ntdgy <ntdgy2001@gmail.com>
2022-10-09 17:23:34 +0800

    add AddResponseHeaderFilter

---
+++ src/main/java/tank/ooad/fitgub/rest/AddResponseHeaderFilter.java
@@ -0,0 +1,25 @@
+package tank.ooad.fitgub.rest;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+//仅限生产环境
+// TODO: delete this file before launch
+@Component
+public class AddResponseHeaderFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        httpServletResponse.addHeader("Access-Control-Allow-Origin","*");
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+}
+
