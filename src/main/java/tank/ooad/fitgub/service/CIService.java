df11ada800fe09d0efd674ee5bc73e9c9696ed14
ntdgy <ntdgy2001@gmail.com>
2022-11-01 22:10:39 +0800

    add CI test version

---
+++ src/main/java/tank/ooad/fitgub/service/CIService.java
@@ -0,0 +1,82 @@
+package tank.ooad.fitgub.service;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.async.ResultCallback;
+import com.github.dockerjava.api.command.CreateContainerResponse;
+import com.github.dockerjava.api.command.ExecCreateCmdResponse;
+import com.github.dockerjava.core.command.ExecStartResultCallback;
+import org.yaml.snakeyaml.Yaml;
+import tank.ooad.fitgub.entity.ci.CI;
+import tank.ooad.fitgub.entity.ci.Job;
+import tank.ooad.fitgub.entity.ci.Step;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class CIService {
+    public void runCI(String path) throws InterruptedException {
+        Yaml yaml = new Yaml();
+        try {
+            InputStream inputStream = new FileInputStream(path);
+            Map<String, Object> obj = yaml.load(inputStream);
+            var ci = new CI();
+            ci.name = (String) obj.get("name");
+            var jobs = (List<Object>) obj.get("jobs");
+            for (var job : jobs) {
+                var jobMap = (Map<String, Object>) job;
+                var jobObj = new Job();
+                jobObj.name = (String) jobMap.get("name");
+                jobObj.runs_on = (String) jobMap.get("runs_on");
+                var steps = (List<Object>) jobMap.get("steps");
+                for (var step : steps) {
+                    var stepMap = (Map<String, Object>) step;
+                    var stepObj = new Step();
+                    stepObj.name = (String) stepMap.get("name");
+                    // make run split by enter
+                    var run = (String) stepMap.get("run");
+                    stepObj.run = Arrays.asList(run.split(","));
+//                stepObj.run = (List<String>) stepMap.get("run");
+                    jobObj.steps.add(stepObj);
+                }
+                ci.jobs.add(jobObj);
+            }
+            DockerClientService dockerClientService = new DockerClientService();
+            //连接docker服务器
+            DockerClient client = dockerClientService.connectDocker();
+            System.out.println(ci.jobs.size());
+            for (var job : ci.jobs) {
+                CreateContainerResponse container = client.createContainerCmd("dgy/ci:v0.1").withCmd("sleep", "3").exec();
+                client.startContainerCmd(container.getId()).exec();
+                System.out.println("container id: " + container.getId());
+                for (var step : job.steps) {
+                    System.out.println(step.run);
+                    for(var run : step.run){
+                        ExecCreateCmdResponse execCreateCmdResponse = client.execCreateCmd(container.getId())
+                                .withAttachStdout(true)
+                                .withAttachStderr(true)
+                                .withCmd("sh", "-c", run)
+                                .exec();
+                        client.execStartCmd(execCreateCmdResponse.getId()).withDetach(false).withTty(true)
+                                .exec(new ResultCallback.Adapter<>()).awaitCompletion();
+                    }
+                }
+                client.stopContainerCmd(container.getId()).exec();
+                client.removeContainerCmd(container.getId()).exec();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }catch (Exception e){
+
+        }
+
+
+
+
+
+    }
+}
---
+++ src/main/java/tank/ooad/fitgub/service/DockerClientService.java
@@ -0,0 +1,72 @@
+package tank.ooad.fitgub.service;
+
+import com.alibaba.fastjson.JSONObject;
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.CreateContainerResponse;
+import com.github.dockerjava.api.model.*;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.DockerClientConfig;
+import com.github.dockerjava.core.DockerClientImpl;
+import com.github.dockerjava.transport.DockerHttpClient;
+
+import java.util.Properties;
+
+import static com.github.dockerjava.api.model.HostConfig.newHostConfig;
+
+public class DockerClientService {
+
+    public DockerClient connectDocker() {
+        DockerClientConfig dockerClientConfig = DefaultDockerClientConfig.createDefaultConfigBuilder()
+                .withDockerHost("tcp://192.168.1.3:2375").build();
+//        DockerClient dockerClient = DockerClientBuilder.getInstance("tcp://192.168.1.3:2375").build();
+        DockerClient dockerClient = DockerClientBuilder.getInstance(dockerClientConfig).build();
+        Info info = dockerClient.infoCmd().exec();
+        String infoStr = JSONObject.toJSONString(info);
+        System.out.println("docker的环境信息如下：=================");
+        System.out.println(info);
+        return dockerClient;
+    }
+
+    public CreateContainerResponse createContainers(DockerClient client, String containerName, String imageName) {
+        //映射端口8088—>80
+//        ExposedPort tcp80 = ExposedPort.tcp(80);
+//        Ports portBindings = new Ports();
+//        portBindings.bind(tcp80, Ports.Binding.bindPort(8088));
+
+        //重定向标准输出
+//        Bind bind = new Bind(filePath, new Volume("/var/jenkins_home"));
+
+
+
+        return client.createContainerCmd(imageName)
+                .withName(containerName)
+                .withHostConfig(newHostConfig()).exec();
+    }
+
+    private static Properties loadProperties() {
+        Properties properties = new Properties();
+        try {
+            properties.load(DockerClientService.class.getClassLoader().getResourceAsStream("docker.properties"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return properties;
+    }
+
+    public void startContainer(DockerClient client, String containerId) {
+        client.startContainerCmd(containerId).exec();
+    }
+
+
+    public void stopContainer(DockerClient client, String containerId) {
+        client.stopContainerCmd(containerId).exec();
+    }
+
+    public void removeContainer(DockerClient client, String containerId) {
+        client.removeContainerCmd(containerId).exec();
+    }
+
+
+}
+
