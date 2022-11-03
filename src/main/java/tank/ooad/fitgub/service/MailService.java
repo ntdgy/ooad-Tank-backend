7ab6b0b9a415082e93d020cb1d8d0f3457853fc8
dgy <ntdgy2001@gmail.com>
2022-11-03 14:23:43 +0800

    fix send email using python

--- src/main/java/tank/ooad/fitgub/service/MailService.java
+++ src/main/java/tank/ooad/fitgub/service/MailService.java
@@ -4,7 +4,9 @@ import jakarta.mail.*;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
 import java.util.Properties;
 
 @Component
@@ -15,38 +17,49 @@ public class MailService {
     private static final String SMTP_AUTH_PWD = "fc447471-1cb7-4914-b15f-daa7fd609255";
 
     public String sendMail(String receiver, String subject, String content) {
-        try {
-            Properties props = new Properties();
-            props.put("mail.smtp.host", SMTP_HOST_NAME);
-            props.put("mail.smtp.auth", "true");
-            props.put("mail.smtp.starttls.enable", "true");
-            props.put("mail.smtp.port", SMTP_HOST_PORT);
-            props.put("mail.smtp.socketFactory.port", SMTP_HOST_PORT);
-            Session session = Session.getInstance(props, new Authenticator() {
-                protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication(SMTP_AUTH_USER, SMTP_AUTH_PWD);
-                }
-            });
-            Transport transport = session.getTransport("smtp");
-            MimeMessage msg = new MimeMessage(session);
-            msg.setFrom(new InternetAddress(SMTP_AUTH_USER));
-            msg.setRecipients(MimeMessage.RecipientType.TO, new Address[]{new InternetAddress(receiver)});
-            msg.setSubject(subject);
-            msg.setText(content);
-//            msg.saveChanges();
+        String url = "http://127.0.0.1:8090";
+        String uuid = "7a095c01-0930-403c-85bb-41bbe10eb89c";
 
-            Transport.send(msg);
+        Map<String,String> map = Map.of("receiver", receiver, "subject", subject,
+                "content", content, "uuid", uuid);
+
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.postForObject(url, map, String.class);
+        return result;
+//        try {
+//            Properties props = new Properties();
+//            props.put("mail.smtp.host", SMTP_HOST_NAME);
+//            props.put("mail.smtp.auth", "true");
+//            props.put("mail.smtp.starttls.enable", "true");
+//            props.put("mail.smtp.port", SMTP_HOST_PORT);
+//            props.put("mail.smtp.socketFactory.port", SMTP_HOST_PORT);
+//            Session session = Session.getInstance(props, new Authenticator() {
+//                protected PasswordAuthentication getPasswordAuthentication() {
+//                    return new PasswordAuthentication(SMTP_AUTH_USER, SMTP_AUTH_PWD);
+//                }
+//            });
+//
+//            MimeMessage msg = new MimeMessage(session);
+//            msg.setFrom(SMTP_AUTH_USER);
+//            msg.setRecipients(MimeMessage.RecipientType.TO, receiver);
+//            msg.setSubject(subject);
+//            msg.setText(content);
+//            msg.saveChanges();
+//
+////            Transport.send(msg);
+//            Transport transport = session.getTransport("smtp");
 //            transport.connect(SMTP_HOST_NAME, SMTP_HOST_PORT, SMTP_AUTH_USER, SMTP_AUTH_PWD);
 //            transport.sendMessage(msg, msg.getAllRecipients());
-            return "success";
-        } catch (Exception e) {
-            e.printStackTrace();
-            return e.getMessage();
-        }
+//            transport.close();
+//            return "success";
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return e.getMessage();
+//        }
     }
 
     public String sendVerificationCode(String receiver, String code) {
-        String subject = "FitGub Verification Code";
+        String subject = "XynHub Verification Code";
         String content = """
                 您的验证码是: %s
                 请在 5 分钟内输入验证码完成登录。
