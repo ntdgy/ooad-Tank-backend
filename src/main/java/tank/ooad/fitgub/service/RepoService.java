76837c7f7511bdf06b0334cf8f51bc802108dd3b
dgy <ntdgy2001@gmail.com>
2022-11-30 17:39:09 +0800

    fix repo get private

--- src/main/java/tank/ooad/fitgub/service/RepoService.java
+++ src/main/java/tank/ooad/fitgub/service/RepoService.java
@@ -403,6 +403,7 @@ public class RepoService {
                        repo.owner_id as repo_owner_id, uo.name as repo_owner_name, uo.email as repo_owner_email
                 from repo
                          join users uo on repo.owner_id = uo.id
+                where repo.visible = 0
                 order by random()
                 limit 1;
                 """, Repo.mapper);
