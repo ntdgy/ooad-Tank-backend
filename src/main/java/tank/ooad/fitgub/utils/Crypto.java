f17aef630339c7db363359e2a6e82baf66e474af
dgy <ntdgy2001@gmail.com>
2022-11-30 20:47:40 +0800

    update hashPasswd

--- src/main/java/tank/ooad/fitgub/utils/Crypto.java
+++ src/main/java/tank/ooad/fitgub/utils/Crypto.java
@@ -8,6 +8,9 @@ import java.security.MessageDigest;
 public class Crypto {
 
     private static final String salt = "fRaNkss";
+
+    private static final int SALT_LENGTH = 16;
+
     public static String hashPassword(String raw) {
         return getSHA1(getSHA1(raw) + salt);
     }
@@ -16,10 +19,16 @@ public class Crypto {
         return DigestUtils.sha1Hex(raw);
     }
 
-    public static String generateVerificationCode(){
+    public static String hashPassword(String raw, String id) {
+        String newSalt = getSHA1(id + salt);
+        String newRaw = getSHA1(newSalt.substring(0, SALT_LENGTH) + raw + newSalt.substring(SALT_LENGTH, SALT_LENGTH * 2));
+        return getSHA1(newRaw + newSalt);
+    }
+
+    public static String generateVerificationCode() {
         StringBuilder code = new StringBuilder();
         for (int i = 0; i < 6; i++) {
-            code.append((int)(Math.random() * 10));
+            code.append((int) (Math.random() * 10));
         }
         return code.toString();
     }
